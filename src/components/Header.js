35e9ad53cb42f5fc724c9e55a1e4ab29bf7dacb3
Karin Ljunggren <kaljunggren@gmail.com>
2021-05-19 22:34:46 +0200

    main sections

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="site_header">
+      <div className="logo_text">
+        <h2>My App</h2>
+      </div>
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/MainContent.js
@@ -0,0 +1,10 @@
+const MainContent = () => {
+  return (
+    <section className="main_content">
+      <article>
+        <h1>Hello world</h1>
+      </article>
+    </section>
+  );
+};
+export default MainContent;
